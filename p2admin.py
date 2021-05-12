6c60ecc1987d5be6ab1621ceea2f4ba4fcb745c5
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2021-05-11 22:34:25 -0600

    Update p2admin.py

--- p2admin.py
+++ p2admin.py
@@ -3,7 +3,7 @@ cur = []
 conn = []
 
 
-def grant():
+def grant():  # todo
     if uinfo == {'mail': 'not logged in'}:
         print('You need to login first')
         return
@@ -16,17 +16,67 @@ def grant():
     if not(auth[0]):
         print("You don't have permissions to use this function")
         return
-    pass
+    arg = input(
+        'Provide the e-mail of the user you want to grant permissions to: ')
+    cur.execute('''SELECT * FROM users WHERE mail = %s''', (arg,))
+    user = cur.fetchone()
+    if not(user):
+        print('The user was not found')
+        return
+    arg = input(
+        'Provide the name of the credential profile you want to grant to the user: ')
+    cur.execute('''SELECT * FROM cred WHERE name = %s''', (arg,))
+    cred = cur.fetchone()
+    if not(cred):
+        print('The credential profile was not found')
+        return
+    try:
+        # mod_users(u_id int, sub boolean, reps int, sub_date date, per int, is_banned boolean, changer varchar)
+        cur.execute('''SELECT * FROM mod_users(%s, %s, %s, %s, %s, %s, %s)''',
+                    (user[0], user[3], user[5], user[6], cred[0],  user[8], uinfo['mail']))
+        conn.commit()
+        print('The user has been granted the permissions')
+    except:
+        conn.rollback()
+        print('Something went wrong with the connection')
 
 
-def newCred():
+def newCred():  # tested
     if uinfo == {'mail': 'not logged in'}:
         print('You need to login first')
         return
     if not(uinfo['admin']):
         print('You need to be an administrator to create credential profiles')
         return
-    pass
+    cred_name = input('Provide a name for the credential: ')
+    cur.execute('''SELECT name FROM cred WHERE name = %s''', (cred_name,))
+    if cur.fetchone():
+        print('Please, use a name that has not been used before')
+        return
+    creds = {'p1': {'desc': "Profile can modify the information of any album or song?: ", 'val': False},
+             'p2': {'desc': "Prfile can disable any albums or songs?: ", 'val': False},
+             'p3': {'desc': "Block unsubscribed users?: ", 'val': False},
+             'p4': {'desc': "Cancel user subscriptions?: ", 'val': False},
+             'p5': {'desc': "Deactivate users registered as artists?: ", 'val': False},
+             'p6': {'desc': "Grant this kind of permissions (access to this function)?: ", 'val': False},
+             'p7': {'desc': "Can get reports from the platform?: ", 'val': False},
+             'p8': {'desc': "Can see the change logs?: ", 'val': False}, }
+    for i in creds.keys():
+        print('Awnser y for yes or n for no: ')
+        print('The profile can')
+        arg = input(creds[i]['desc'])
+        if arg == 'y':
+            creds[i]['val'] = True
+    try:
+        cur.execute(
+            '''INSERT INTO cred (p1, p2, p3, p4, p5, p6, p7, p8, name) values(%s, %s, %s, %s, %s, %s, %s, %s, %s)''',
+            (creds['p1']['val'], creds['p2']['val'], creds['p3']['val'], creds['p4']['val'],
+             creds['p5']['val'], creds['p6']['val'], creds['p7']['val'], creds['p8']['val'], cred_name))
+        print('The profile has been created')
+        conn.commit()
+    except:
+        conn.rollback()
+        print('Something went wrong with the connection')
 
 
 def getCreds():  # tested
@@ -46,7 +96,9 @@ def getCreds():  # tested
             print('     Grant permissions: ' + str(i[6]))  # todo
             print('     Get reports: ' + str(i[7]))  # todo
             print('     Check logs: ' + str(i[8]))  # todo
+        conn.commit()
     except:
+        conn.rollback()
         print('Something went wrong with the connection')
     pass
 
@@ -112,3 +164,36 @@ def bitacora():
         print("You don't have permissions to use this function")
         return
     pass
+
+
+def ban():  # tested
+    if uinfo == {'mail': 'not logged in'}:
+        print('You need to login first')
+        return
+    cur.execute(
+        '''SELECT p3 FROM users INNER JOIN cred ON users.credenciales = credid WHERE uid = %s''', (uinfo['uid'], ))
+    auth = cur.fetchone()
+    if not(auth):
+        print("You don't have permissions to use this function")
+        return
+    if not(auth[0]):
+        print("You don't have permissions to use this function")
+        return
+    arg = input('Provide the e-mail of the user you want to ban: ')
+    cur.execute('''SELECT * FROM users WHERE mail = %s''', (arg,))
+    res = cur.fetchone()
+    if not(res):
+        print('The user was not found')
+        return
+    if res[3]:
+        print('The user is subscribed so it cannot be banned')
+        return
+    try:
+        # mod_users(u_id int, sub boolean, reps int, sub_date date, per int, is_banned boolean, changer varchar)
+        cur.execute('''SELECT * FROM mod_users(%s, %s, %s, %s, %s, %s, %s)''',
+                    (res[0], res[3], res[5], res[6], res[7], True, uinfo['mail']))
+        conn.commit()
+        print('The user has been banned')
+    except:
+        conn.rollback()
+        print('Something went wrong with the connection')
